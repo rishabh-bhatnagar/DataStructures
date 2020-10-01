e7f15f0cea6677f0603476e86203756a95885448
Rishabh Bhatnagar <bhatnagarrishabh4@gmail.com>
2020-10-01 15:04:12 +0530

    Add BST Traversals In GoLang
    Signed-off-by: Rishabh Bhatnagar <bhatnagarrishabh4@gmail.com>

--- GoLang/BST.go
+++ GoLang/BST.go
@@ -35,6 +35,53 @@ func (root *BSTNode) insert(data int) {
 	}
 }
 
+func (root *BSTNode) inorder() []int {
+	var left, right []int
+	if root.Left != nil {
+		left = root.Left.inorder()
+	}
+	if root.Right != nil {
+		right = root.Right.inorder()
+	}
+	return append(append(left, root.Data), right...)
+}
+
+func (root *BSTNode) preorder() []int {
+	var left, right []int
+	if root.Left != nil {
+		left = root.Left.inorder()
+	}
+	if root.Right != nil {
+		right = root.Right.inorder()
+	}
+	return append(append([]int{root.Data}, left...), right...)
+}
+
+func (root *BSTNode) postorder() []int {
+	var left, right []int
+	if root.Left != nil {
+		left = root.Left.inorder()
+	}
+	if root.Right != nil {
+		right = root.Right.inorder()
+	}
+	return append(append(left, right...), root.Data)
+}
+
+func (root *BSTNode) levelOrder() (seq []int) {
+	queue := []*BSTNode{root}
+	for len(queue) > 0 {
+		currNode := queue[0]
+		queue = queue[1:]
+		if currNode != nil {
+			seq = append(seq, currNode.Data)
+			queue = append(queue, currNode.Left)
+			queue = append(queue, currNode.Right)
+		}
+	}
+	return seq
+}
+
 func main() {
 	root := BSTNode{Data: 12}
 	root.insert(1)
